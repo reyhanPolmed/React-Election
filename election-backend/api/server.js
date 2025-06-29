daa5caa2f8db007ebf7844eead56d041378bc0c5
reyhanPolmed <jhonsonrahmat@gmail.com>
2025-06-30 00:07:40 +0700

    pembaruan frontend cors

--- election-backend/api/server.js
+++ election-backend/api/server.js
@@ -24,7 +24,7 @@ app.use(
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL || "http://localhost:3000", "https://*.vercel.app", /\.vercel\.app$/],
+    origin: [process.env.FRONTEND_URL || "http://localhost:3000", "https://election-theta.vercel.app/", /\.vercel\.app$/],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
